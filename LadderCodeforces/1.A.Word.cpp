ea867aed887b6aa2f8e62edebff702d9b11f746e
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2020-08-15 16:10:33 -0500

    Update 1.A.Word.cpp

--- LadderCodeforces/1.A.Word.cpp
+++ LadderCodeforces/1.A.Word.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 #include <iostream>
 #include <string>
-#define optimizar_io ios_base::sync_with_stdio(0); cin.tie(0);
  
 int main(){
   
-	string palabra;
-  int n, mayusculas=0, minusculas=0;
-  cin>>palabra;
+string palabra;
+int n, mayusculas=0, minusculas=0;
+cin>>palabra;
  	
- 	n=palabra.size();
-  vector<int> nuevo(n,0);
+ n=palabra.size();
+ vector<int> nuevo(n,0);
  
  	for(int i=0; i<palabra.size(); i++){
     	if(palabra[i]=='A'|| palabra[i]=='B'|| palabra[i]=='C'|| palabra[i]=='D'|| palabra[i]=='E'|| palabra[i]=='F'|| palabra[i]=='G'|| palabra[i]=='H'|| palabra[i]=='I'|| palabra[i]=='J'|| palabra[i]=='K'|| palabra[i]=='L'|| palabra[i]=='M'|| palabra[i]=='N'|| palabra[i]=='O'|| palabra[i]=='P'|| palabra[i]=='Q'|| palabra[i]=='R'|| palabra[i]=='S'|| palabra[i]=='T'|| palabra[i]=='U'|| palabra[i]=='V'|| palabra[i]=='W'|| palabra[i]=='X'|| palabra[i]=='Y'|| palabra[i]=='Z'){
