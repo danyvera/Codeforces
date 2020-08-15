99f57693cfa47bb3b29721eb0af3882fe92e189a
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2020-08-15 17:43:01 -0500

    Create A. A+B.cpp

---
+++ ConcursoDonaldKnuth2020/A. A+B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+
+ //link to find the problem in Codeforces: https://codeforces.com/group/BQ6WfEWeXy/contest/288759/problem/A
+ 
+int main(){
+  long long A,B, suma;
+ 
+  cin>> A >> B;
+  suma=A+B;
+  cout << suma;
+}
