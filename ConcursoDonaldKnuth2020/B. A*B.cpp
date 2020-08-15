42672104e880ef8777f793c397c95c2b04efea81
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2020-08-15 17:44:38 -0500

    Create B. A*B.cpp

---
+++ ConcursoDonaldKnuth2020/B. A*B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+ 
+ //link to find the problem in Codeforces: https://codeforces.com/group/BQ6WfEWeXy/contest/288759/problem/B
+ 
+int main(){
+  long long A,B, multiplicacion;
+ 
+  cin>> A >> B;
+  multiplicacion=A*B;
+  cout << multiplicacion;
+}
